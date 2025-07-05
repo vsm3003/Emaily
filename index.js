81dced30fe1abb2884357a7b6f5ec4dd79160df7
vsm3003 <vidmoraes@gmail.com>
2025-07-05 17:28:08 -0400

    Removido a pasta config e o arquivo keys.js e trocado para as váriaveis de ambiente no Render

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
 const app = express();
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },  accessToken => {
-        console.log(accessToken);
-    })
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  accessToken => {
+    console.log(accessToken);
+  })
 );
 
 app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
 );
 
-const PORT = process.env.PORT  || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
