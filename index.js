fa3cc57c21e694f7683553534f3b3e1d57b88d8c
vsm3003 <vidmoraes@gmail.com>
2025-07-05 19:15:15 -0400

    Adicionados os arquivos authRoutes.js e passport.js e atualizado o arquivo index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-require('dotenv').config();
-
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-  },
-  accessToken => {
-    console.log(accessToken);
-  })
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
